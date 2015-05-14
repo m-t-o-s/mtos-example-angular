addd0394ba3b0edc9de1ebe3d4281107fd788ce4
Andrew Davis <diffalot@diff.mx>
2015-05-13 22:39:17 -0500

    readability

--- client/js/index.js
+++ client/js/index.js
@@ -6,8 +6,11 @@ angular.module('mtos', [
   require('./modules/webtorrent').name,
   require('angular-ui-router')
 ])
+
 .constant('version', require('../../package.json').version)
+
 .controller('WelcomeCtrl', require('./welcome/welcome.controller'))
+
 .config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise("/debug")
   $stateProvider
