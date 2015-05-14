01cceae694f500b73f506a919c0faa775964cc69
Andrew Davis <diffalot@diff.mx>
2015-05-13 22:37:08 -0500

    add ui-router, use welcome as debug

--- client/js/index.js
+++ client/js/index.js
@@ -3,7 +3,17 @@
 var angular = require('angular')
 
 angular.module('mtos', [
-  require('./modules/webtorrent').name
+  require('./modules/webtorrent').name,
+  require('angular-ui-router')
 ])
 .constant('version', require('../../package.json').version)
 .controller('WelcomeCtrl', require('./welcome/welcome.controller'))
+.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/debug")
+  $stateProvider
+  .state('debug', {
+    url: "/debug",
+    controller: 'WelcomeCtrl',
+    templateUrl: "js/welcome/welcome.template.html"
+  })
+})
