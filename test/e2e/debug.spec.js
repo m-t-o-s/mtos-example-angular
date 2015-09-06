1f9e4e811d91a2a4d41c4bd8ad35974e8dd5d772
Andrew Davis <diffalot@diff.mx>
2015-09-06 17:20:57 -0500

    sucessfully load credentials for alice from a zip file

--- test/e2e/debug.spec.js
+++ test/e2e/debug.spec.js
@@ -1,10 +1,31 @@
 /* global describe, it browser, expect */
 'use strict'
 
+var path = require('path')
+
 describe('Debug View', function () {
+
   it('should have a title', function () {
     browser.get('/#/debug')
 
     expect(browser.getTitle()).toEqual('mtos')
   })
+
+  it('should load credentials from a zip file', function (done) {
+    var fileToUpload = '../test-credentials/alice.zip'
+    var absolutePath = path.resolve(__dirname, fileToUpload)
+
+    var fileInput = element(by.css('.db-backups > input[type="file"]'))
+
+    fileInput.sendKeys(absolutePath)
+
+    element.all(by.repeater('user in db.users'))
+    .first().element(by.binding('user.username'))
+    .getText().then(function (text) {
+      expect(text).toBe('alice')
+      done()
+    })
+
+  })
+
 })
