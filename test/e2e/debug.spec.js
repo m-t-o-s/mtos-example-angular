d5fbea1e2f6130c6aecff829e8530c7a5187864f
Andrew Davis <diffalot@diff.mx>
2015-09-06 15:02:23 -0500

    WIP adding protractor tests

---
+++ test/e2e/debug.spec.js
@@ -0,0 +1,10 @@
+/* global describe, it browser, expect */
+'use strict'
+
+describe('Debug View', function () {
+  it('should have a title', function () {
+      browser.get('/#/debug')
+
+      expect(browser.getTitle()).toEqual('mtos')
+    })
+})
