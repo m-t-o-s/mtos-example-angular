e2e71a9c7536e4f5cb61af5a77856e1c077a5bf8
Andrew Davis <diffalot@diff.mx>
2015-09-07 10:45:14 -0500

    start three browsers and load alice, bob, and eve's keys

--- test/e2e/debug.spec.js
+++ test/e2e/debug.spec.js
@@ -6,12 +6,14 @@ var path = require('path')
 describe('Debug View', function () {
 
   it('should have a title', function () {
-    browser.get('/#/debug')
 
+    browser.get('/#/debug')
     expect(browser.getTitle()).toEqual('mtos')
+
   })
 
-  it('should load credentials from a zip file', function (done) {
+  it('should load alice\'s credentials from a zip file', function (done) {
+
     var fileToUpload = '../test-credentials/alice.zip'
     var absolutePath = path.resolve(__dirname, fileToUpload)
 
@@ -23,6 +25,50 @@ describe('Debug View', function () {
     .first().element(by.binding('user.username'))
     .getText().then(function (text) {
       expect(text).toBe('alice')
+    })
+    .then(function () {
+      done()
+    })
+
+  })
+
+  it('should load bob\'s credentials from a zip file', function (done) {
+
+    var fileToUpload = '../test-credentials/bob.zip'
+    var absolutePath = path.resolve(__dirname, fileToUpload)
+
+    var browserBob = browser.forkNewDriverInstance(true)
+    var elementBob = browserBob.element
+
+    var fileInput = elementBob(by.css('.db-backups > input[type="file"]'))
+    fileInput.sendKeys(absolutePath)
+
+    elementBob.all(by.repeater('user in db.users'))
+    .first().element(by.binding('user.username'))
+    .getText().then(function (text) {
+      expect(text).toBe('bob')
+    })
+    .then(function () {
+      done()
+    })
+
+  })
+
+  it('should load eve\'s credentials from a zip file', function (done) {
+
+    var fileToUpload = '../test-credentials/eve.zip'
+    var absolutePath = path.resolve(__dirname, fileToUpload)
+
+    var browserEve = browser.forkNewDriverInstance(true)
+    var elementEve = browserEve.element
+
+    var fileInput = elementEve(by.css('.db-backups > input[type="file"]'))
+    fileInput.sendKeys(absolutePath)
+
+    elementEve.all(by.repeater('user in db.users'))
+    .first().element(by.binding('user.username'))
+    .getText().then(function (text) {
+      expect(text).toBe('eve')
     })
     .then(function () {
       done()
